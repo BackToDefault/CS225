59738a64b89b9da890bd706ee9a726534caa95d5
Andrew Gordon <BackToDefault@gmail.com>
2016-04-28 13:14:06 -0700

    Lab 6 fully operational

--- lab6.sh
+++ lab6.sh
@@ -3,31 +3,28 @@ LABCOUNT=0
 XMLCOUNT=0
 
 IFS=$'\n'
-XML=$( grep -q "filename" ~/medialab/media.xml | cut -f2 -d ">" | cut -f1 -d "<" )
+XML=$( grep "filename" ~/medialab/media.xml | cut -f2 -d ">" | cut -f1 -d "<" | sort | uniq ) 2>&1
 LAB=$( ls -x1 ~/medialab/ )
 
 echo "medialab files not in media.xml:"
 for LABFILE in $LAB ;do
-	if ! ( grep -q $LABFILE ~/medialab/media.xml ) && ! [[ -z $LABFILE ]] ;then
-		echo "$LABFILE"
-		(( LABCOUNT++ ))
-	else
-		continue
-	fi
+   if ! ( grep -q $LABFILE ~/medialab/media.xml ) && ! [[ -z $LABFILE ]] ;then
+      echo "$LABFILE"
+      (( LABCOUNT++ ))
+   else
+      continue
+   fi
 done
 echo "There were $LABCOUNT file(s) not found in media.xml"
 
 
 echo "media.xml files not in medialab:"
 for XMLFILE in $XML ;do
-	if ! ( ls ~/medialab/ | grep -q "$XMLFILE" ) ;then
-		echo "$XMLFILE"
-		(( XMLCOUNT++ ))
-#		continue
-	else
-#		echo "$XMLFILE"
-#		(( XMLCOUNT++ ))
-		continue
-	fi
+   if ! [ -e ~/medialab/$XMLFILE ] ;then
+      echo "$XMLFILE"
+      (( XMLCOUNT++ ))
+   else
+      continue
+   fi
 done
 echo "There were $XMLCOUNT file(s) not found in medialab"

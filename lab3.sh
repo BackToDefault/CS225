42fc82af0a62c337b8d0f634bbb14822c4b2d8f5
Andrew Gordon <BackToDefault@gmail.com>
2016-04-28 05:57:20 -0700

    Workable Lab 6

--- lab3.sh
+++ lab3.sh
@@ -2,8 +2,11 @@
 if [ $UID -lt 500 ]
 then
 	echo "It is not safe to run this script as root"
-else [ $UID -ge 500 ]
+elif [ $UID -ge 500 ]
+then
 	echo "Valid user"
 	HOME=$(grep $UID /etc/passwd | cut -d ":" -f6)
 	du -sh $HOME
+else
+	echo "How did you get here?"
 fi
--- lab6.sh
+++ lab6.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
-XML=$( grep -q "filename" ~/medialab/media.xml | cut -f2 -d ">" | cut -f1 -d "<" )
+LABCOUNT=0
+XMLCOUNT=0
+
 IFS=$'\n'
+XML=$( grep -q "filename" ~/medialab/media.xml | cut -f2 -d ">" | cut -f1 -d "<" )
 LAB=$( ls -x1 ~/medialab/ )
 
 echo "medialab files not in media.xml:"
 for LABFILE in $LAB ;do
-	if ( grep -q $LABFILE $XML ) ;then
-		continue
+	if ! ( grep -q $LABFILE ~/medialab/media.xml ) && ! [[ -z $LABFILE ]] ;then
+		echo "$LABFILE"
+		(( LABCOUNT++ ))
 	else
-		echo "$LABFILE" && LABCOUNT=$(( $MEDIACOUNT + 1 ))
 		continue
 	fi
 done
 echo "There were $LABCOUNT file(s) not found in media.xml"
 
 
-#echo "media.xml files not in medialab:"
-#for XMLFILE in $XML ;do
-#	if ( ls ~/medialab | grep -q "$XMLFILE" ) ;then
+echo "media.xml files not in medialab:"
+for XMLFILE in $XML ;do
+	if ! ( ls ~/medialab/ | grep -q "$XMLFILE" ) ;then
+		echo "$XMLFILE"
+		(( XMLCOUNT++ ))
 #		continue
-#	else
+	else
 #		echo "$XMLFILE"
-#		XMLCOUNT=$(( $XMLCOUNT + 1 ))
-#	fi
-#done
-#echo "There were $XMLCOUNT file(s) not found in medialab"
+#		(( XMLCOUNT++ ))
+		continue
+	fi
+done
+echo "There were $XMLCOUNT file(s) not found in medialab"
